addafb5ba9f6b0572723ada458cdba035fddd80d
wendysegura <118@holbertonschool.com>
2020-01-15 10:49:20 -0800

    added more keys

--- dummyfile.py
+++ dummyfile.py
@@ -1,2 +1,5 @@
 access_key_id: ABCDEFGHIGKLMNOPQRST
 secret_access_key: BHG%@Abc12345&*Tnsl#lksdfj%LKfetasdf$@#0
+
+access_key_id: ABCDEFGHIGKLMNOPUJFY
+secret_access_key: BHG%@Abc12345&*Tnsl#lksdfj%LKf34534df$@#0
