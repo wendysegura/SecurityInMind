a71763e373ff9c93deebf00912554fd5e04d2a8c
wendysegura <118@holbertonschool.com>
2020-01-15 11:37:16 -0800

    dummy keys

--- dummyfile.py
+++ dummyfile.py
@@ -1,5 +1,2 @@
-access_key_id: ABCDEFGHIGKLMNOPQRST
-secret_access_key: BHG%@Abc12345&*Tnsl#lksdfj%LKfetasdf$@#0
-
-access_key_id: ABCDEFGHIGKLMNOPUJFY
-secret_access_key: BHG%@Abc12345&*Tnsl#lksdfj%LKf34534df$@#0
+Key: 'ABCDEFGHIGKLMNOPQRST'
+Secret: 'BHG%@Abc12345&*Tnsl#lksdfj%LKfetasdf$@#0'
